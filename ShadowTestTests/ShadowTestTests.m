1bc18dbc083d0ce4de5394e41b71d688e3f3d7d7
王钱钧 <arthur.wang@yoho.cn>
2014-09-01 19:37:14 +0800

    add project

---
+++ ShadowTestTests/ShadowTestTests.m
@@ -0,0 +1,34 @@
+//
+//  ShadowTestTests.m
+//  ShadowTestTests
+//
+//  Created by 王钱钧 on 14-9-1.
+//  Copyright (c) 2014年 王钱钧. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ShadowTestTests : XCTestCase
+
+@end
+
+@implementation ShadowTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
